435f9fdf3968c78f9cdb6fe71dad5931b1710651
LopesX <baraodeitacare@hotmail.com>
2017-05-30 23:41:08 -0300

    Ajuste de HTML e CSS

--- portfolios/views.py
+++ portfolios/views.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import DadosPessoais
 
 # Create your views here.
 
+
 def portfolio_exibir(request):
-	return render(request, 'portfolios/portfolio_exibir.html', {})
+
+    pessoa = DadosPessoais.objects.all()
+    context = {'pessoa': pessoa}
+    return render(request, 'portfolios/portfolio_exibir.html', context)
